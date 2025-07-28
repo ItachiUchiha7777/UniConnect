bc175ff198f273a05e3cb5fd69060746922542d3
Rohit Gusain <98213628+ItachiUchiha7777@users.noreply.github.com>
2025-07-28 22:27:10 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -4,6 +4,6 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
   withCredentials: true,
 });
-console.log('API Base URL:', API.defaults.baseURL);
+// console.log('API Base URL:', API.defaults.baseURL);
 
 export default API;
