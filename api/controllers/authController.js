e29e092188eff399035ef94ae89ee3a039cb2007
Rohit Gusain <98213628+ItachiUchiha7777@users.noreply.github.com>
2025-07-28 22:04:54 +0530

    login SameSite None

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -58,7 +58,7 @@ exports.loginUser = async (req, res) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
